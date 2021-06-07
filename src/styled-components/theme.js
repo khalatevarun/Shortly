13db6178124d317987babfee4ea40903ecf94fac
Varun Khalate <khalatevarun@gmail.com>
2021-06-08 00:27:34 +0530

    add theme colors

---
+++ src/styled-components/theme.js
@@ -0,0 +1,7 @@
+export const Cyan = '#2acfcf';
+export const DarkViolet = '#3b3054';
+export const Red = '#f46262';
+export const Gray = '#bfbfbf';
+export const Grayish = '#bfbfbf';
+export const VeryDarkBlue = '#35323e';
+export const VeryDarkViolet = '#232127';
