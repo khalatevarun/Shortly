e898a1c63116cf0966372b7e3b394fae93c7a6f6
Varun Khalate <khalatevarun@gmail.com>
2021-06-08 11:31:02 +0530

    add button styling

--- src/global-components/RoundButton.js
+++ src/global-components/RoundButton.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 export const RoundButton = styled.button`
   background-color: ${(props) => props.theme.Cyan};
   border-radius: 25px;
-  font-size: 1.15em;
-  padding: 8px 18px;
+  font-size: 0.85em;
+  padding: 8px 20px;
   border: none;
   font-weight: 700;
   color: white;
