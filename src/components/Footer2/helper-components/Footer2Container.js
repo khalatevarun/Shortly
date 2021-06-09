ceae632e82376a4026499e6c585325627a16e63e
Varun Khalate <khalatevarun@gmail.com>
2021-06-09 17:32:10 +0530

    complete desktop design

--- src/components/Footer2/helper-components/Footer2Container.js
+++ src/components/Footer2/helper-components/Footer2Container.js
@@ -3,10 +3,12 @@ import boost_desktop from '../../../images/boost_desktop.svg';
 export const Footer2Container = styled.div`
   background-color: ${(props) => props.theme.VeryDarkBlue};
   display: flex;
-  padding: 60px;
+  padding: 100px;
   justify-content: center;
   align-items: center;
   flex-direction: row;
+  justify-content: space-around;
+  align-items: flex-start;
 
   @media (max-width: ${(props) => props.theme.mobile_breakpoint}) {
     padding: 30px 0px;
---
+++ src/components/Footer2/helper-components/Name.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Name = styled.div`
+  color: white;
+  font-size: 2em;
+  font-weight: 700;
+  flex: 0.5;
+`;
