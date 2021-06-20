381e4d82a1a72ac67de55f61329d985a0b9fad6c
Varun Khalate <khalatevarun@gmail.com>
2021-06-20 17:57:09 +0530

    update Div1 styling

--- src/components/Hero/helper-components/Div1.js
+++ src/components/Hero/helper-components/Div1.js
@@ -4,7 +4,7 @@ export const Div1 = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  margin: 60px 100px;
+  margin: 60px 200px 60px 100px;
   align-items: baseline;
 
   @media (max-width: ${(props) => props.theme.mobile_breakpoint}) {
